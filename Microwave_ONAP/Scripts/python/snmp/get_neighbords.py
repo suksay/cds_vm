5fca4dc4680617c9a6206d37dab3ab875517c346
Silas Monney <suksay11@gmail.com>
2021-04-02 11:10:27 +0200

    Update get_neighbords.py

--- Microwave_ONAP/Scripts/python/snmp/get_neighbords.py
+++ Microwave_ONAP/Scripts/python/snmp/get_neighbords.py
@@ -1,10 +1,22 @@
+
 import json
 import quicksnmp
 from pysnmp import hlapi
 from pyconfig import *
+from pyconfig import _MW_INVARIANT_ID_, _HUAWEI_MW_VERSION_ID_, _NEC_MW_VERSION_ID_
 import os
-from aai_requests import *
+from contextlib import contextmanager
+import sys
+import ipaddress
+from multiprocessing import Pool
+from  aai_requests import *
+import json
 from jinja2 import Environment, FileSystemLoader
+
+
+global devices
+
+devices = dict()
 
 #Checking workspace
 cwd = os.getcwd()
@@ -12,182 +24,163 @@ if cwd.split('/')[-1]!='snmp' : os.chdir("Scripts/python/snmp/")
 
 env = Environment(loader=FileSystemLoader('./aai_templates/'))
 
-p_interface = env.get_template('p-interface.json')
-l_interface = env.get_template('l-interface.json')
-physical_link = env.get_template('physical-link.json')
+#Network parameters
+network = "172.20.183.128"
+mask = "25"
 
-def get_hua_id_from_inventory(ne_id, hosts):
-    for ne in hosts:
-        if ne['ne_id'] == ne_id :
-            return ne['device_id']
+hua_ne_id =  ".1.3.6.1.4.1.2011.2.25.3.40.2.1.1.0"
 
+def create_dict(*args):
+    return dict({i:eval(i) for i in args})
 
-def get_huawei_neighbors(ip_address, hosts):
-    links = []
-    serie_fulloutdor = [92, 138, 151, 160]
-    hosts_ids = [ne['device_id'] for ne in hosts]
+@contextmanager
+def no_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
 
-    ne_serie = str(quicksnmp.get_oid(str(ip_address), hua_ne_serie,credentials)[0][-1])
-    if int(ne_serie) in serie_fulloutdor:
-        """
-            Case of full outdor NE
-        """
-        if_links = quicksnmp.get_table_oids(str(ip_address),hua_link_search_table, credentials)
-        for link in if_links :
-            link_i = dict()
-            link_i["local_int_index"]  = ""
-            link_i["local_intf"] = str(link[1][-1]) +"/"+ str(link[2][-1]) +"/"+ str(link[3][-1])
-            link_i["neighbor"] =  get_hua_id_from_inventory(str(link[4][-1]), hosts)
-            link_i["neighbor_intf"] = str(link[5][-1]) +"/"+ str(link[6][-1]) +"/"+ str(link[7][-1])
-            links.append(link_i)    
-
-    neighbors=quicksnmp.get_table(ip_address, lldp_table_hua,credentials) #For NE Interface with LLDP
-    for link in neighbors:
-        link_i = dict()
-        if str(link[0][1]) != "Not received":
-            link_i["local_int_index"]= link[0][0][-1]
-            link_i["local_intf"]= str(link[0][0][-4])+'/'+str(link[0][0][-3])+'/'+str(link[0][0][-2])
-            link_i["neighbor"]= '0x'+(link[0][1].prettyPrint()).replace('-','').lower()
-            neighbor=[chassis for chassis in hosts_ids if link_i["neighbor"][:-4] in chassis]
-            if neighbor != [] :
-                link_i["neighbor"]=neighbor[0]
-                n_int = str(link[1][1]).split(' ')
-                if len(n_int) == 3 : link_i["neighbor_intf"]=n_int[1]
-                else : link_i["neighbor_intf"]=n_int[0]
-                links.append(link_i)
-    return links 
-
-def get_nec_neighbords(ip_address, hosts):
-    links = []
-    neighbors=quicksnmp.get_table(ip_address, lldp_table_named_oid,credentials)
-    hosts_ids = [ne['device_id'] for ne in hosts]
-
-    for link in neighbors:
-        link_i = dict()
-        """
-            link[0][1]  : value of lldpremChassisId 
-            link[1][1]  : value of lldpremPortDesc
-
-        """
-        if str(link[1][1]) != "Not received" :
-            link_i["local_int_index"]= link[0][0][-2]
-            loc_port_desc='.1.0.8802.1.1.2.1.3.7.1.4.'+str(link_i["local_int_index"]) #Can't do it with MIBS because of an exceeding ValueConstraint on NEC devices
-            link_i["local_intf"]= str(quicksnmp.get_oid(ip_address,loc_port_desc,credentials)[0][1])
-            link_i["neighbor"]= link[0][1].prettyPrint()
-            neighbor=[chassis for chassis in hosts_ids if link_i["neighbor"][:-4] in chassis]
-            if neighbor != [] :
-                link_i["neighbor"]=neighbor[0]
-                #for exemple  if lldpremPortDesc = "FastEthernet 1/255/4 Interface"
-                # n_int = str(link[1][1]).split(' ') = ['FastEthernet', '1/255/4', 'Interface']
-                n_int = str(link[1][1]).split(' ')
-                if len(n_int) == 3 : link_i["neighbor_intf"]=n_int[1]
-                else : link_i["neighbor_intf"]=n_int[0]
-                if link_i["local_intf"] != 'NMS':
-                    links.append(link_i)
-    return links
-
-
-def neighborships_aai_data_normalize(neighborships_data, devices_list):
-
-    def extract_opposite_site(nodeB_id, nodeB_int,nodeA_id, nodeA_int):
-        for element in neighborships_data[nodeB_id]:
-            if nodeA_id == element['neighbor'] and nodeA_int == element['neighbor_intf'] and nodeB_int == element['local_intf'] :
-                nodeB_int_index = element['local_int_index']
-                neighborships_data[nodeB_id].pop(neighborships_data[nodeB_id].index(element))
-
-                return nodeB_int_index
+#SNMP Ping + Chassis ID + Hostname + Vendor retrieving function
+def get_hostname(ip):
+    """
+    inputs:
+        ip (str) : Device IP address
     
-    devices = list()
-    for ne in devices_list:
-        devices.append({ne['device_id']:ne['hostname']})
-
-    aai_data = dict()
-    aai_data['neighborships'] = list()
-    #links = []
-    for device_id in neighborships_data:
-        if neighborships_data[device_id] != [] :
-            for element in neighborships_data[device_id] :
-                nodeB_index = extract_opposite_site(element["neighbor"],element["neighbor_intf"],device_id,element["local_intf"])
-                if nodeB_index != None :
-                    """
-                    #Create Device A p-interface and l-interface
-                    URL_DEVICE_A_P_INTF = URL_GET_PNFS +'/pnf/{pnf_name}/p-interfaces/p-interface/{interface_name}'.format(pnf_name=device_id, interface_name=element['local_int_index'])
-                    p_interf_A_data = json.loads(p_interface.render(interface_name=element['local_int_index'], equip_identifier=element["local_intf"]))
-
-                    URL_DEVICE_A_L_INTF = URL_DEVICE_A_P_INTF + '/l-interfaces/l-interface/{interface_id}'.format(interface_id=element['local_int_index'])
-                    l_interf_A_data = json.loads(l_interface.render(interface_id=element['local_int_index'], interface_name=element["local_intf"]))
-
-                    req_p_interf_A = put_request(URL_DEVICE_A_P_INTF, p_interf_A_data)
-                    req_l_interf_A = put_request(URL_DEVICE_A_L_INTF, l_interf_A_data)
-
-                    #Create Device B p-interface and l-interface
-                    URL_DEVICE_B_P_INTF = URL_GET_PNFS +'/pnf/{pnf_name}/p-interfaces/p-interface/{interface_name}'.format(pnf_name=element["neighbor"], interface_name=nodeB_index)
-                    p_interf_B_data = json.loads(p_interface.render(interface_name=nodeB_index, equip_identifier=element["neighbor_intf"]))
-                    
-                    URL_DEVICE_B_L_INTF = URL_DEVICE_B_P_INTF + '/l-interfaces/l-interface/{interface_id}'.format(interface_id=nodeB_index)
-                    l_interf_B_data = json.loads(l_interface.render(interface_id=nodeB_index, interface_name=element["neighbor_intf"]))
-
-                    req_p_interf_B = put_request(URL_DEVICE_B_P_INTF, p_interf_B_data)
-                    req_l_interf_B = put_request(URL_DEVICE_B_L_INTF, l_interf_B_data)
-
-                    #Make neighborship with physical-link
-                    link_name = devices[device_id] +'_'+element["local_int_index"]+'-'+devices[element["neighbor"]]+'_'+nodeB_index
-                    URL_P_LINK = URL_GET_PHYSICAL_LINK + '/physical-link/{link_name}'.format(link_name=link_name)
-                    physical_link_data = json.loads(physical_link.render(link_name=link_name, device_A_id=device_id , device_A_interf_name=element['local_int_index'], device_B_id=element["neighbor"], device_B_interf_name=nodeB_index))
-                    req_link = put_request(URL_P_LINK, physical_link_data)
-
-                    """
-                    neighbor = dict()
-                    neighbor['nodeA_id'] = device_id
-                    neighbor['nodeA_name'] = devices[device_id]
-                    neighbor['nodeA_intf_index'] = element['local_int_index']
-                    neighbor['nodeA_intf'] = element['local_intf']
-
-                    neighbor['nodeB_id'] = element['neighbor']
-                    neighbor['nodeB_name'] = devices[element["neighbor"]]
-                    neighbor['nodeB_intf_index'] = nodeB_index
-                    neighbor['nodeB_intf'] = element["neighbor_intf"]
-
-                    aai_data['neighborships'].append(neighbor)
-
-    return aai_data
-
-def get():
-    #Get Devices list 
-    cwd = os.getcwd()
-    if cwd.split('/')[-1]!='snmp' : os.chdir("Scripts/python/snmp/")
-
+    outputs:
+        Tuple : (Chassis ID, [Device information])
+    """ 
+    global devices
+    def create_dict(*args):
+        return dict({i:eval(i) for i in args})
     try:
-        f=open("inventory.json",)
-        devices=json.load(f)
-        f.close()
+        with no_stdout():
+            vendor=quicksnmp.get(str(ip), snmp_ping,credentials)
     except:
-        devices = dict()
+        return False
+    else:
+        address=str(ip)
+        vendor=str(vendor[0][1])
+        id=''
+        if vendor=="Optix product" : 
+            vendor="huawei"
+            ne_id = str(quicksnmp.get_oid(str(ip), hua_ne_id,credentials)[0][-1])
+            hostname = str(quicksnmp.get_oid(str(ip), huawei_sysname,credentials)[0][1])
+            id='0x'+str(quicksnmp.get(str(ip), lldp_hua_id,credentials)[0][1]).replace('-','')
+            return (id,[address,hostname,vendor,ne_id])            
+        elif vendor=='ipe' : 
+            vendor="nec-ipa"
+            hostname = str(quicksnmp.get(str(ip), sysName_named_oid,credentials)[0][1])
+            id=quicksnmp.get(str(ip), lldp_id,credentials)[0][1].prettyPrint()
+        elif vendor=='-':
+            try :
+                #Checking the "Equipment Type" OID that return an integer >10000 for VR products
+                if int(str(quicksnmp.get_oid(address,nec_vr_type,credentials)[0][1]))>10000:
+                    vendor="nec-vr"
+                    hostname = str(quicksnmp.get(str(ip), sysName_named_oid,credentials)[0][1])
+                    id=quicksnmp.get(str(ip), lldp_id,credentials)[0][1].prettyPrint()
+                else: raise ValueError('')
+            except:
+                return False
+        if id=='': return False
+        return (id,[address,hostname,vendor])
 
-    neighborships = dict()
 
-    for device in devices:
-        links = []
-        address=device['address']
-        vendor=device['vendor']
-        """
-        Different SNMP processing according to vendor.
-        """
-        if 'nec' in vendor:
-            nec_links = get_nec_neighbords(address, devices)
-            links.extend(nec_links)
-        if vendor=='huawei':
-            hua_links = get_huawei_neighbors(address, devices)
-            links.extend(hua_links)
-            
-        if links != [] : neighborships[id]=links 
+
+list_addr=ipaddress.IPv4Network(network+'/'+mask).hosts()
+
+#Building a pool of subprocesses for each IP address (e.g : for /24 network, creates 254 subprocesses)
+"""
+This operation may require analysis in order to get the best optimized value here.
+The subprocessing segmentation here is therefore guaranteed up to 1024 addresses.
+"""
+a_pool = Pool(2**(32-int(mask))-2)
+
+#Subprocessing get_hostname() function for each addresses
+db_hostname=a_pool.map(get_hostname,list_addr)
+
+#Filtering successfully polled addresses
+db=[host for host in db_hostname if host != False]
+
+#Building and updating/creating Inventory file
+for id,infos in db:
+    address=infos[0]
+    hostname=infos[1]
+    vendor=infos[2]
+    if vendor == "huawei" :
+        ne_id = infos[3]
+        devices[id]=create_dict('address','hostname','vendor', 'ne_id')
+    else : 
+        devices[id]=create_dict('address','hostname','vendor')
+
+
+### Save Host in AAI Database ###
+aai_devices_data = dict()
+aai_devices_data['devices'] = list()
+
+devices_model = env.get_template('device.json')
+old_devices_model = env.get_template('old_devices.json')
+pnf_model = env.get_template('pnf.json') 
+
+
+def update_aai(devices):
+
+    def create_device(device_id, device_info, isExist=False):
+        URL_DEVICE = URL_GET_DEVICES + '/device/{device_id}'.format(device_id = device_id)
+
+        if isExist == True :
+            device_data = json.loads(old_devices_model.render(device_id=device_id, operational_status="down", resource_version=device_info))
+        else :
+            if device_info['vendor'] == 'huawei' :
+                device_data = json.loads(devices_model.render(device_id=device_id, device_name=device_info['hostname'], vendor=device_info['vendor'], ipv4=device_info['address'], description="We use model-customization-id for NE ID ", operational_status ="up", ne_id=device_info['ne_id'], model_invariant_id=_MW_INVARIANT_ID_, model_version_id=_HUAWEI_MW_VERSION_ID_))
+            else:
+                device_data = json.loads(devices_model.render(device_id=device_id, device_name=device_info['hostname'], vendor=device_info['vendor'], ipv4=device_info['address'], description="We use model-customization-id for NE ID ", operational_status ="up", model_invariant_id=_MW_INVARIANT_ID_, model_version_id=_NEC_MW_VERSION_ID_))
+
+        req_device = put_request(URL_DEVICE, device_data)
+
+    def create_pnf(device_id, device_info, isExist=False):
+        URL_DEVICE = URL_GET_DEVICES + '/device/{device_id}'.format(device_id = device_id)
+        URL_PNF = URL_GET_PNFS + '/pnf/{pnf_name}'.format(pnf_name = device_id )
+
+        if isExist == True :
+            pnf_data = json.loads(old_devices_model.render(device_id=device_id, operational_status="down", resource_version=device_info))
+        else :
+            if device_info['vendor'] == 'huawei' :
+                pnf_data = json.loads(pnf_model.render(device_id=device_id, device_name=device_info['hostname'], selflink=URL_DEVICE, equip_type="microwave", vendor=device_info['vendor'], operational_status ="up", model_invariant_id=_MW_INVARIANT_ID_, model_version_id=_HUAWEI_MW_VERSION_ID_))
+            else:
+                pnf_data = json.loads(pnf_model.render(device_id=device_id, device_name=device_info['hostname'], selflink=URL_DEVICE, equip_type="microwave", vendor=device_info['vendor'], operational_status ="up", model_invariant_id=_MW_INVARIANT_ID_, model_version_id=_NEC_MW_VERSION_ID_))
+
+        req_pnf = put_request(URL_PNF, pnf_data)
+
     
 
-    aai_neighbords_datas = neighborships_aai_data_normalize(neighborships, devices)
-    print(aai_neighbords_datas)
+    #Get current Devices (and Pnfs) in AAI
+    try:
+        current_devices = get_request(URL_GET_DEVICES)[1]['device']
+        current_devices = {node['device-id']:node['resource-version'] for node in current_devices}
+    except:
+        current_devices = dict()
     
+    for id in devices:
+        devices[id]['device_id'] = id
+        aai_devices_data['devices'].append(devices[id])
+
+        if id in current_devices.keys() :
+            del(current_devices[id])
+            create_device(id, devices[id])
+            create_pnf(id, devices[id])
+
+    for id in current_devices:
+        create_device(id,current_devices[id],True)
+        create_pnf(id,current_devices[id],True)
 
 
-    #print(neighborships)
-
+    
+fp = open('inventory.json', 'w+') 
+json.dump(aai_devices_data, fp)
+fp.close()
+print('Current Available Devices')
+print(aai_devices_data)
 
